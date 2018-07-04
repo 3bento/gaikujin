09e715da2a5ea0f4064ff78cdf1143bd59996890
Kenny Code <kenny030217@gmail.com>
2018-07-03 23:58:40 -0300

    Added DAO classes, refactored the JqplSumAndAvgTest

---
+++ src/br/com/kennycode/gaikujin/dao/TransactionDAO.java
@@ -0,0 +1,27 @@
+package br.com.kennycode.gaikujin.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.kennycode.gaikujin.model.TypeTransaction;
+
+public class TransactionDAO {
+
+	private EntityManager em;
+	
+	public TransactionDAO(EntityManager em) {
+		this.em = em;
+	}
+
+	public List<Double> getAverageByTypeAndDate(TypeTransaction type) {
+		//Account account = em.find(Account.class, getRandomAccountId());
+		//String jpql = "select sum(t.value) from Transaction t where t.account = :pAccount and t.type = :pType order by t.value desc";
+		String jpql = "select avg(t.value) from Transaction t where t.type = :pType group by year(t.date), month(t.date), day(t.date)";
+		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
+		// PURCHASES
+		query.setParameter("pType", type);
+		return query.getResultList();
+	}
+}
--- src/br/com/kennycode/gaikujin/test/JpqlAvgAndGroupByTest.java
+++ src/br/com/kennycode/gaikujin/test/JpqlAvgAndGroupByTest.java
@@ -1,15 +1,12 @@
 package br.com.kennycode.gaikujin.test;
 
-import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
+import br.com.kennycode.gaikujin.dao.TransactionDAO;
 import br.com.kennycode.gaikujin.model.TypeTransaction;
 import br.com.kennycode.gaikujin.util.GenerateDatabase;
 import br.com.kennycode.gaikujin.util.JpaManager;
@@ -23,36 +20,22 @@ import br.com.kennycode.gaikujin.util.JpaManager;
 public class JpqlAvgAndGroupByTest {
 	public static void main(String[] args) {
 
-		// generate the data before the test work.
 		GenerateDatabase.initialize(50);
 
 		EntityManager em = JpaManager.getConnection();
 		em.getTransaction().begin();
 
-		//Account account = em.find(Account.class, getRandomAccountId());
-		//String jpql = "select sum(t.value) from Transaction t where t.account = :pAccount and t.type = :pType order by t.value desc";
-		String jpql = "select avg(t.value) from Transaction t where t.type = :pType group by year(t.date), month(t.date), day(t.date)";
-		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-		// PURCHASES
-		query.setParameter("pType", TypeTransaction.PURCHASES);
-		List<Double> avgOfPurchases = query.getResultList();
-		// PAYMENTS
-		query.setParameter("pType", TypeTransaction.PAYMENTS);
-		List<Double> avgOfPayments = query.getResultList();
-		// RECEIPTS
-		query.setParameter("pType", TypeTransaction.RECEIPTS);
-		List<Double> avgOfReceipts = query.getResultList();
-		// SALSE
-		query.setParameter("pType", TypeTransaction.SALES);
-		List<Double> avgOfSales = query.getResultList();
+		TransactionDAO dao = new TransactionDAO(em);
+		
+		List<Double> avgOfPayments = dao.getAverageByTypeAndDate(TypeTransaction.PAYMENTS); 
+		List<Double> avgOfPurchases = dao.getAverageByTypeAndDate(TypeTransaction.PURCHASES); 
+		List<Double> avgOfReceipts = dao.getAverageByTypeAndDate(TypeTransaction.RECEIPTS); 
+		List<Double> avgOfSales = dao.getAverageByTypeAndDate(TypeTransaction.SALES); 
 
 		printTotalByCategory(TypeTransaction.PURCHASES, avgOfPurchases);
 		printTotalByCategory(TypeTransaction.PAYMENTS, avgOfPayments);
 		printTotalByCategory(TypeTransaction.RECEIPTS, avgOfReceipts);
 		printTotalByCategory(TypeTransaction.SALES, avgOfSales);
-
-		// TODO - Use the max() from PSQL!!! ;D
-		// TODO - Use the count() from PSQL!!!!!!!!! ;DDDDD
 
 		em.getTransaction().commit();
 		em.clear();
@@ -69,17 +52,17 @@ public class JpqlAvgAndGroupByTest {
 		}
 	}
 
-	private static int getRandomAccountId() {
-		Random rand = new Random();
-		EntityManager em = JpaManager.getConnection();
-		em.getTransaction().begin();
-		// query to get all accounts
-		String jpql = "select a from Account a";
-		Query query = em.createQuery(jpql);
-		// get the size of accounts
-		int accountId = rand.nextInt(query.getResultList().size()) + 1;
-		em.getTransaction().commit();
-		em.close();
-		return accountId;
-	}
+//	private static int getRandomAccountId() {
+//		Random rand = new Random();
+//		EntityManager em = JpaManager.getConnection();
+//		em.getTransaction().begin();
+//		// query to get all accounts
+//		String jpql = "select a from Account a";
+//		Query query = em.createQuery(jpql);
+//		// get the size of accounts
+//		int accountId = rand.nextInt(query.getResultList().size()) + 1;
+//		em.getTransaction().commit();
+//		em.close();
+//		return accountId;
+//	}
 }
